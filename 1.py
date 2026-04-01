6e69a27a28e81ebe7f5debaf2af1476366f1e796
Rakshith <harshith.shetty2002@gmail.com>
2026-04-01 16:29:34 +0530

    Learning on

---
+++ 1.py
@@ -0,0 +1,25 @@
+print('hello world')
+
+x = 100;
+
+print(x)
+
+name = 'Shetty'
+print(type(name))
+
+age = 24
+
+age_one = float(age)
+print(age_one)
+
+lname = 'shetty'
+fname = str(x)
+
+print(lname+ fname)
+
+a = 10
+b = 3
+print( a % b)
+
+name = input('Enter your name')
+print(name)
---
+++ 2.py
@@ -0,0 +1,4 @@
+
+
+age = input('age: ')
+print(age)
